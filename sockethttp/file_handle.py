cfebe0dde8d4877f9a91d99ecc20144508e2bbb9
wednesdaygogo <527963988@qq.com>
2023-02-27 18:11:43 +0800

    增加从xlsx文件读payload功能，增加内置测试流量黑流量命令功能

--- sockethttp/file_handle.py
+++ sockethttp/file_handle.py
@@ -2,11 +2,11 @@ import os
 import random
 import base64
 import download_picture
+from openpyxl import load_workbook
 
 def get_filename(path):
     filelist = os.listdir(path)
     return filelist
-
 def read_file(path):
     with open(path,encoding='utf-8',errors='ignore') as f:
         return f.read()
@@ -24,7 +24,7 @@ def write_file(path):
         count = count + 1
     print('共处理文件名{}个'.format(count))
 
-def generate_random_str(ip,port,length=16,encode=None,filename="N"):
+def generate_random_str(ip,port,length=16,encode=None,filename="N"):#生成payload为随机字符串的http报文集合，可选择是否进行base64编码
     random_str = ''
     base_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()_-+=[{]};:\'\",<.>/?|\\'
     str_len = len(base_str) - 1
@@ -37,7 +37,7 @@ def generate_random_str(ip,port,length=16,encode=None,filename="N"):
         payload = generate_http_payload(ip,port,random_str,filename)
     return payload
 
-def generate_internet_str(ip,port,file,encode=None):
+def generate_internet_str(ip,port,file,encode=None):#根据nft处理后的元数据生成现网的http报文集合，可选择是否进行base64编码
     content = read_file("./post_payload/{}".format(file))
     if encode == "base64":
         internet_str = base64_encode(content)
@@ -46,7 +46,7 @@ def generate_internet_str(ip,port,file,encode=None):
         payload = generate_http_payload(ip, port, content, file)
     return payload
 
-def generate_picture(ip,port,keyword=None,pages=10):
+def generate_picture(ip,port,keyword=None,pages=10):#自动在百度图片中爬取指定关键字的图片并生成由图片组成的payload集合
     if os.path.exists("picture"):
         pass
     else:
@@ -56,12 +56,12 @@ def generate_picture(ip,port,keyword=None,pages=10):
             generate_http_payload(ip,port,read_file("picture\\"+filename))
 
 
-def base64_encode(str):
+def base64_encode(str):#工具函数，对指定串进行base64编码
     str_encode = base64.b64encode(str.encode('utf-8'))
     str_encode = str_encode.decode('utf-8')
     return str_encode
 
-def generate_http_payload(ip,port,payload,filename):
+def generate_http_payload(ip,port,payload,filename):#将处理好的数据填写进指定的http模板中
     content = read_file('http.txt')
     if port == 80:
         host = ip
@@ -70,9 +70,10 @@ def generate_http_payload(ip,port,payload,filename):
     http = content.replace("%ip",host).replace("%length",str(len(payload))).replace("%payload",payload).replace("%file",filename)
     return http
 
-def whitefile_write(ip,port,type,length=16,num=2000,encode=None):
+def whitefile_write(ip,port,type,length=16,num=2000,encode=None):#将生成好的http报文批量写入指定文件夹
+    '''生成随机字符串文件可选择是否base64编码'''
     if type == "random":
-        path = './white_list/base64/'
+        path = './white_list/simple_post/'
         path = path.strip()
         path = path.rstrip("\\")
         isexist = os.path.exists(path)
@@ -82,8 +83,9 @@ def whitefile_write(ip,port,type,length=16,num=2000,encode=None):
             with open(path+str(i)+'.txt','w',encoding='utf-8') as f:
                 f.write(generate_random_str(ip,port,length,encode,str(i)))
                 print("成功生成文件:{}".format(path+str(i)+'.txt'))
+    #生成现网流量文件可选择是否base64编码
     elif type == "internet":
-        path = './white_list/internet_post/'
+        path = './white_list/simple_post/'
         path = path.strip()
         path = path.rstrip("\\")
         isexist = os.path.exists(path)
@@ -94,11 +96,64 @@ def whitefile_write(ip,port,type,length=16,num=2000,encode=None):
             with open(path+filename,'w',encoding='utf-8') as f:
                 f.write(generate_internet_str(ip,port,filename,encode))
                 print("成功生成文件:{}".format(path+filename))
+    #根据getclass.py生成的xlsx表格生成序列化测试数据
+    elif type == "php_serilize":
+        path = './black_list/php_serilize_payload/'
+        path = path.strip()
+        path = path.rstrip("\\")
+        isexist = os.path.exists(path)
+        if not isexist:
+            os.makedirs(path)
+        workbook_object = load_workbook(filename='phpggc_payload.xlsx')
+        sheet = workbook_object.worksheets[0]
+        max_row_num = sheet.max_row
+        for i in range(2,max_row_num + 1):
+            with open(path+sheet[i][0].value.replace('/','_') + '.txt','w',encoding='utf-8') as f:
+                f.write(generate_http_payload(ip,port,sheet[i][1].value.strip(),sheet[i][0].value))
+                print("成功生成文件:{}".format(path+sheet[i][0].value.replace('/','_') + '.txt'))
+    elif type == "xxe":
+        path = './black_list/aes_lanjie_payload/'
+        path = path.strip()
+        path = path.rstrip("\\")
+        isexist = os.path.exists(path)
+        if not isexist:
+            os.makedirs(path)
+        workbook_object = load_workbook(filename='lanjie.xlsx')
+        sheet = workbook_object.worksheets[0]
+        max_row_num = sheet.max_row
+        for i in range(2,max_row_num + 1):
+            with open(path+str(sheet[i][0].value) + '.txt','w',encoding='utf-8') as f:
+                f.write(generate_http_payload(ip,port,sheet[i][1].value,str(sheet[i][0].value)))
+                print("成功生成文件:{}".format(path+str(sheet[i][0].value) + '.txt'))
+    elif type == "xlsx":
+        path = './black_list/aes_lanjie_payload/'
+        path = path.strip()
+        path = path.rstrip("\\")
+        isexist = os.path.exists(path)
+        if not isexist:
+            os.makedirs(path)
+        workbook_object = load_workbook(filename='lanjie.xlsx')
+        sheet = workbook_object.worksheets[0]
+        max_row_num = sheet.max_row
+        for i in range(2,max_row_num + 1):
+            with open(path+str(sheet[i][0].value) + '.txt','w',encoding='utf-8') as f:
+                f.write(sheet[i][1].value)
+                print("成功生成文件:{}".format(path+str(sheet[i][0].value) + '.txt'))
 
 
 
 if __name__ == '__main__':
-    whitefile_write("99.99.99.88",80,"random",1000,2000,"base64")
+    # 获取行数
+    # workbook_object = load_workbook(filename='phpggc_payload.xlsx')
+    # sheet = workbook_object.worksheets[0]
+    # max_row_num = sheet.max_row
+    # for i in range(2,max_row_num + 1):
+    #     payload_dic = {'name':'','payload':''}
+    #     payload_dic['name'] = sheet[i][0].value
+    #     payload_dic['payload'] = sheet[i][1].value.strip()
+    #     print(payload_dic)
+    whitefile_write("99.99.99.88",80,"xlsx")
+
     # x= base64_encode("ss())(Lp;ph88;\\@#$RF>?\":L{PO\$)P{IPK\#\)J\)\$NMM\:Kls")
     # print(x)
 
--- sockethttp/sockethttp.py
+++ sockethttp/sockethttp.py
@@ -1,10 +1,25 @@
 import socket
 import os
 import argparse
-
-import threading
+import sys
 from concurrent.futures import ThreadPoolExecutor,as_completed
 
+from openpyxl import load_workbook
+
+black_payload_path ={
+    'php_code_injection':'./black_list/phpcodeinject_small_4k_payload/',
+    'php_serilize':'./black_list/php_serilize_payload/',
+    'xml_injection':'./black_list/xxe_payload/',
+    'xray_php_serilize_payload':'./black_list/xray_php_serilize_payload/',
+    'aes_payload':'./black_list/aes_lanjie_payload/'
+}
+
+white_payload_path = {
+    'white_base64':'./white_list/white_payload_base64/',
+    'white_picture':'./white_list/white_payload_picture/',
+    'white_internet_post':'./white_list/internet_post_payload/',
+    'normal_string':'./white_list/normal_string_post_payload/',
+}
 
 def read_dir(path):
     filelist = []
@@ -16,7 +31,11 @@ def read_file(path):
     with open(path,'rb') as f:
         content = f.read()
     return content
+
 def copy_file(src,dst):
+    isexist = os.path.exists(dst)
+    if not isexist:
+        os.makedirs(dst)
     cmd = 'copy {} {}'.format(src,dst)
     os.system(cmd)
 def send(ip,port,content):
@@ -55,7 +74,7 @@ def main(url,port,content,file):
         else:
             print(file + ' 已拦截')
             return 1
-            #copy_file(file,'.\\text\\')
+            #copy_file(file,'.\\command-wubao\\')
 
 
 def muti_thread_test(url,port,dir,threads):
@@ -84,35 +103,89 @@ def muti_thread_test(url,port,dir,threads):
     print("超时数量：" + str(out_time))
     print("检测率："+str(success/success+faild+out_time))
 
-def single_thread_test(url,port,file_dir):
-
+def send_from_xlsx(url,port,path):
+    if os.path.splitext(path)[-1] != '.xlsx':
+        print('-x 参数需为xlsx后缀文件')
+        sys.exit()
     success = 0
+    count = 0
+    workbook_object = load_workbook(filename=path)
+    sheet = workbook_object.worksheets[0]
+    max_row_num = sheet.max_row
+    for i in range(2, max_row_num + 1):
+        try:
+            content = sheet[i][0].value.lstrip().encode("utf-8")
+        except AttributeError:
+            continue
+        try:
+            count = count + 1
+            result = send(url, int(port), content)
 
-    dir = read_dir(file_dir)
+            # print(result)
+        except TimeoutError:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 连接超时')
+            # copy_file(file,'.\\outtime\\')
+            continue
+        except ConnectionResetError:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 已拦截')
+            success = success + 1
+            continue
+        except ConnectionAbortedError:
+            continue
+        # print(result)
+        if result.find('403') == -1:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 未拦截')
+            # copy_file(file,'.\\aes_bypass\\')
+        else:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 已拦截')
+            success = success + 1
+            # copy_file(file,'.\\command-wubao\\')
+    print("一共发送样本数量：{}".format(max_row_num))
+    print("拦截数：{}".format(success))
+    print("未拦截数：{}".format(max_row_num - success))
+    print("检测率：" + str(success / max_row_num))
+
+
+def single_thread_test(url,port,file_dir):
+    dir = []
+    success = 0
+    count = 0
+    if file_dir == 'white_all':
+        for key,value in white_payload_path.items():
+            dir += read_dir(value)
+    elif file_dir == 'black_all':
+        for key,value in black_payload_path.items():
+            dir += read_dir(value)
+    else:
+        if file_dir[-1] != '\\' and file_dir[-1] != '/':
+            file_dir = file_dir + '/'
+        dir = read_dir(file_dir)
     total = len(dir)
     for file in dir:
         content = read_file(file)
         try:
+            count = count + 1
             result  = send(url,int(port),content)
+
             #print(result)
         except TimeoutError:
-            print(file + ' 连接超时')
+            print('序号:'+str(count) + ' ' + file + ' 连接超时')
             #copy_file(file,'.\\outtime\\')
             continue
         except ConnectionResetError:
-            print(file + ' 已拦截')
+            print('序号:'+str(count) + ' ' + file + ' 已拦截')
             success = success + 1
             continue
         except ConnectionAbortedError:
             continue
         #print(result)
         if result.find('403') == -1:
-            print(file + ' 未拦截')
-            #copy_file(file,'.\\beforechange\\')
+            print('序号:'+str(count) + ' ' + file + ' 未拦截')
+            #copy_file(file,'.\\aes_bypass\\')
         else:
-            print(file + ' 已拦截')
+            print('序号:'+str(count) + ' ' + file + ' 已拦截')
             success = success + 1
-            #copy_file(file,'.\\QMYES\\')
+            #copy_file(file,'.\\command-wubao\\')
     print("一共发送样本数量：{}".format(total))
     print("拦截数：{}".format(success))
     print("未拦截数：{}".format(total-success))
@@ -124,13 +197,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", help="Target ip;Example:10.88.1.220")
     parser.add_argument("-p", "--port", help="Target port;Example:8001")
-    parser.add_argument("-f", "--file", help="Target file;Example:D:\pythonProject\sockethttp\\text")
+    parser.add_argument("-d", "--dir", help="Target file;Example:D:\pythonProject\sockethttp\\text")
     parser.add_argument("-t", "--threads", help="thread number;Example:-t 10代表10个线程")
+    parser.add_argument("-w","--white",help="白流量测试；Example: -w white_base64 表示测试white_base64类型白流量，all测试全部类型")
+    parser.add_argument("-b", "--black", help="黑流量测试；Example: -b php_serilize 表示测试php_serilize类型黑流量，all测试全部类型")
+    parser.add_argument("-x", "--xlsx", help="从xlsx中读取payload测试，要求xlsx文件第一列为payload Example: -x ./xxx.xlsx")
     args = parser.parse_args()
     if args.threads == None:
-        single_thread_test(args.url,int(args.port),args.file)
+        if args.white != None:
+            if args.white == 'all':
+                single_thread_test(args.url,int(args.port),'white_all')
+            else:
+                single_thread_test(args.url,int(args.port),white_payload_path[args.white])
+        if args.black != None:
+            if args.black == 'all':
+                single_thread_test(args.url,int(args.port),'black_all')
+            else:
+                single_thread_test(args.url,int(args.port),black_payload_path[args.black])
+        if args.dir != None:
+            single_thread_test(args.url, int(args.port),args.dir)
+        if args.xlsx != None:
+            send_from_xlsx(args.url,int(args.port),args.xlsx)
+
+
     else:
-        muti_thread_test(args.url,int(args.port),args.file,int(args.threads))
+        muti_thread_test(args.url,int(args.port),args.dir,int(args.threads))
 
     #muti_thread_test(args.url,int(args.port),args.file,int(args.threads))
     # content = read_file('D:\pythonProject\sockethttp\\bypass\\1147.txt')
