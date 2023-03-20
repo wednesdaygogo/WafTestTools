d20ba060716e41ae4e345fa5614510ff08dcb04c
wednesdaygogo <527963988@qq.com>
2023-03-20 18:35:11 +0800

    增加查询上报事件名称功能

--- sockethttp/sockethttp.py
+++ sockethttp/sockethttp.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 from concurrent.futures import ThreadPoolExecutor,as_completed
 
+import requests
 from openpyxl import load_workbook
 
 black_payload_path ={
@@ -144,7 +145,67 @@ def send_from_xlsx(url,port,path):
     print("拦截数：{}".format(success))
     print("未拦截数：{}".format(max_row_num - success))
     print("检测率：" + str(success / max_row_num))
+def send_from_xlsx_plus(url,port,path,column1,column2):
 
+    if os.path.splitext(path)[-1] != '.xlsx':
+        print('-x 参数需为xlsx后缀文件')
+        sys.exit()
+    success = 0
+    count = 0
+    workbook_object = load_workbook(filename=path)
+    sheet = workbook_object.worksheets[0]
+    max_row_num = sheet.max_row
+
+    for i in range(2, max_row_num + 1):
+        try:
+            content1 = str(sheet[i][column1].value).strip('"').replace("\\r\\n","\r\n")
+            content2 = str(sheet[i][column2].value).strip('"').replace("\\r\\n","\r\n")
+            content = content1 + content2
+            content = content.encode("utf-8")
+        except AttributeError:
+            continue
+        try:
+            count = count + 1
+            result = send(url, int(port), content)
+
+            # print(result)
+        except TimeoutError:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 连接超时')
+            # copy_file(file,'.\\outtime\\')
+            continue
+        except ConnectionResetError:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 已拦截  ' + '上报事件:{}'.format(check_eventname_form_waf()))
+            success = success + 1
+            continue
+        except ConnectionAbortedError:
+            continue
+        # print(result)
+        if result.find('403') == -1:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 未拦截')
+            # copy_file(file,'.\\aes_bypass\\')
+        else:
+            print('序号:' + str(count) + ' ' + 'line: '+ str(i) + ' 已拦截  ' + '上报事件:{}'.format(check_eventname_form_waf()) )
+            success = success + 1
+            # copy_file(file,'.\\command-wubao\\')
+    print("一共发送样本数量：{}".format(max_row_num - 1))
+    print("拦截数：{}".format(success))
+    print("未拦截数：{}".format(max_row_num - success))
+    print("检测率：" + str(success / max_row_num))
+
+def check_eventname_form_waf():
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+        'Cookie': 'SID=s%3AX4F1sfbMT6zGopThbAuy6mRgHuAynYQD.IfyCgthHscW7IgTLTseuCvYOxc9L72SWRD2nzKHjiAs; VWPHPUSERID=adm',
+        'Content-Type':'application/json',
+        'Connection':'close',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbSIsImtleSI6IjE2NzkzMDY5MjA5MzktMC4yNTIxMTM0NDUyNDc0MDIxIiwiaWF0IjoxNjc5MzA2OTM1LCJleHAiOjE2NzkzMzU3MzV9.KYfUs7Ph4ZpB4FI9e5xwKq772JRKS0ej4R81SDdMWVk'
+    }
+    data = {"page":1,"limit":1,"isEnglish":0,"searchType":"ad","searchstr":"","auditlinkage_dstip":"","auditlinkage_uv":"","auditlinkage_pv":"","auditlinkage_port":0,"auditlinkage_wafip":"10.51.15.186","filters":[],"chk_time":"on","chk_evt_name":"on","chk_evt_group":"on","chk_evt_level":"on","chk_x_forwarded_for":"on","chk_srcip_str":"on","chk_srcport":"on","chk_dstip_str":128,"chk_dstport":"on","chk_action":"on","chk_rawguid":"on"}
+    url = "http://10.51.15.186/securityeventmonitoring/eventmonlist"
+
+    response = requests.post(url=url, json=data,headers=header)
+    result = response.json()
+    return result['data']['rows'][0]['evt_name']
 
 def single_thread_test(url,port,file_dir):
     dir = []
@@ -194,37 +255,34 @@ def single_thread_test(url,port,file_dir):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--url", help="Target ip;Example:10.88.1.220")
-    parser.add_argument("-p", "--port", help="Target port;Example:8001")
-    parser.add_argument("-d", "--dir", help="Target file;Example:D:\pythonProject\sockethttp\\text")
-    parser.add_argument("-t", "--threads", help="thread number;Example:-t 10代表10个线程")
-    parser.add_argument("-w","--white",help="白流量测试；Example: -w white_base64 表示测试white_base64类型白流量，all测试全部类型")
-    parser.add_argument("-b", "--black", help="黑流量测试；Example: -b php_serilize 表示测试php_serilize类型黑流量，all测试全部类型")
-    parser.add_argument("-x", "--xlsx", help="从xlsx中读取payload测试，要求xlsx文件第一列为payload Example: -x ./xxx.xlsx")
-    args = parser.parse_args()
-    if args.threads == None:
-        if args.white != None:
-            if args.white == 'all':
-                single_thread_test(args.url,int(args.port),'white_all')
-            else:
-                single_thread_test(args.url,int(args.port),white_payload_path[args.white])
-        if args.black != None:
-            if args.black == 'all':
-                single_thread_test(args.url,int(args.port),'black_all')
-            else:
-                single_thread_test(args.url,int(args.port),black_payload_path[args.black])
-        if args.dir != None:
-            single_thread_test(args.url, int(args.port),args.dir)
-        if args.xlsx != None:
-            send_from_xlsx(args.url,int(args.port),args.xlsx)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-u", "--url", help="Target ip;Example:10.88.1.220")
+    # parser.add_argument("-p", "--port", help="Target port;Example:8001")
+    # parser.add_argument("-d", "--dir", help="Target file;Example:D:\pythonProject\sockethttp\\text")
+    # parser.add_argument("-t", "--threads", help="thread number;Example:-t 10代表10个线程")
+    # parser.add_argument("-w","--white",help="白流量测试；Example: -w white_base64 表示测试white_base64类型白流量，all测试全部类型")
+    # parser.add_argument("-b", "--black", help="黑流量测试；Example: -b php_serilize 表示测试php_serilize类型黑流量，all测试全部类型")
+    # parser.add_argument("-x", "--xlsx", help="从xlsx中读取payload测试，要求xlsx文件第一列为payload Example: -x ./xxx.xlsx")
+    # args = parser.parse_args()
+    # if args.threads == None:
+    #     if args.white != None:
+    #         if args.white == 'all':
+    #             single_thread_test(args.url,int(args.port),'white_all')
+    #         else:
+    #             single_thread_test(args.url,int(args.port),white_payload_path[args.white])
+    #     if args.black != None:
+    #         if args.black == 'all':
+    #             single_thread_test(args.url,int(args.port),'black_all')
+    #         else:
+    #             single_thread_test(args.url,int(args.port),black_payload_path[args.black])
+    #     if args.dir != None:
+    #         single_thread_test(args.url, int(args.port),args.dir)
+    #     if args.xlsx != None:
+    #         send_from_xlsx(args.url,int(args.port),args.xlsx)
+    #
+    #
+    # else:
+    #     muti_thread_test(args.url,int(args.port),args.dir,int(args.threads))
 
-
-    else:
-        muti_thread_test(args.url,int(args.port),args.dir,int(args.threads))
-
-    #muti_thread_test(args.url,int(args.port),args.file,int(args.threads))
-    # content = read_file('D:\pythonProject\sockethttp\\bypass\\1147.txt')
-    # print(content)
-    # result = send('99.99.99.88',80,content)
-    # print(result)
+    send_from_xlsx_plus("99.99.99.88",80,"test1.xlsx",6,7)
+    #check_eventname_form_waf()
